3f68bf4d09dfd2cec1e378d7c322e7197c15aa5e
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-09-19 16:52:37 +0900

    react dom import 삭제

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "../css/home.css";
 import Nav from "./Nav";
 
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "../css/nav.css";
 
 const menus = ["Home", "Game", "Rank", "List"];
