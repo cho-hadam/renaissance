1971f416e23d6ab7211b05beba5670923792b827
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-09-19 15:26:23 +0900

    디렉토리명 변경, Nav 추가 등

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../css/home.css";
+import Nav from "./Nav";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <Nav />
+        <main></main>
+      </>
+    );
+  }
+}
+
+export default Home;
---
+++ src/Components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <>
+        <div></div>
+      </>
+    );
+  }
+}
+
+export default Nav;
