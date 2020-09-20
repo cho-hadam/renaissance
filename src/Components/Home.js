b90e1519e5d685f051104b7dea2b5c95e4f3cb1b
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-09-21 02:56:40 +0900

    Home 페이지 구조 잡기 완료

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,7 +7,26 @@ const backgroundColor = {
 
 class Home extends React.Component {
   render() {
-    return <main style={backgroundColor}>home</main>;
+    return (
+      <main style={backgroundColor}>
+        <section className={"Contents"}>
+          <div className={"LeftContent"}>
+            <h1>Renaissance</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum justo enim, ultricies id est nec, eleifend commodo
+              justo. Donec vel felis lacus. Nam eget risus vitae mauris molestie
+              posuere. Praesent facilisis finibus orci id sagittis. Nullam a
+              suscipit neque, eget sollicitudin urna.
+            </p>
+            <button className={"BtnGame"}>Game</button>
+          </div>
+          <div className={"RightContent"}>
+            <img src={require("../assets/image/shop.png")} />
+          </div>
+        </section>
+      </main>
+    );
   }
 }
 
