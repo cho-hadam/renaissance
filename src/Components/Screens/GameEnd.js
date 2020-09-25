feced9c003914c1beba1eea6ea402a1decb76248
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-09-25 15:47:56 +0900

    이름 입력 화면 레이아웃 구성

--- src/Components/Screens/GameEnd.js
+++ src/Components/Screens/GameEnd.js
@@ -1,12 +1,35 @@
 import React from "react";
 
-const backgroundColor = {
-  backgroundColor: "#F5832D",
+const styles = {
+  backgroundColor: {
+    backgroundColor: "#F5832D",
+  },
+  contents: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 };
 
 class List extends React.Component {
   render() {
-    return <main style={backgroundColor}>gameEnd</main>;
+    return (
+      <main style={styles.backgroundColor}>
+        <section style={styles.contents}>
+          <div className={"TopContent"}>
+            <h1>SCORE</h1>
+            <span>2019</span>
+          </div>
+          <div className={"BottomContent"}>
+            <input type={"text"} />
+            <button className={"BtnSubmit"}>확인</button>
+          </div>
+        </section>
+      </main>
+    );
   }
 }
 
