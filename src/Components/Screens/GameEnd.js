0b6d7ee9abc0e6f5d8c99f44a946c8434308949d
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-09-25 15:25:37 +0900

    이름 입력 화면 추가

---
+++ src/Components/Screens/GameEnd.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const backgroundColor = {
+  backgroundColor: "#F5832D",
+};
+
+class List extends React.Component {
+  render() {
+    return <main style={backgroundColor}>game</main>;
+  }
+}
+
+export default List;
