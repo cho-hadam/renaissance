3f0b6d7552a639d99bb99e7696877e4a30759362
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-11-14 19:35:43 +0900

    Home

--- src/Components/Screens/Home.js
+++ src/Components/Screens/Home.js
@@ -1,7 +1,29 @@
 import React from "react";
 
+import "../../css/home.css";
+import ShopImage from "../../assets/image/home/shop-isometric.png";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main id={"HomeContainer"}>
+      <section className={"HomeContents"}>
+        <div className={"LeftContent"}>
+          <h1>Renaissance</h1>
+          <p className={"HomeDescription"}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            justo enim, ultricies id est nec, eleifend commodo justo. Donec vel
+            felis lacus. Nam eget risus vitae mauris molestie posuere. Praesent
+            facilisis finibus orci id sagittis. Nullam a suscipit neque, eget
+            sollicitudin urna.
+          </p>
+          <button className={"BtnGame"}>Game</button>
+        </div>
+        <div className={"RightContent"}>
+          <img src={ShopImage} />
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Home;
