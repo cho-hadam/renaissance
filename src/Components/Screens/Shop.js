7b4779ca1967814bf506e28b46b0ec948bee1b16
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-11-17 22:58:26 +0900

    Menu Card Layout

--- src/Components/Screens/Shop.js
+++ src/Components/Screens/Shop.js
@@ -8,13 +8,20 @@ import storeData from "../../data/store_data.json";
 import locationIcon from "../../assets/images/detail/address.png";
 import callIcon from "../../assets/images/detail/call.png";
 import timeIcon from "../../assets/images/detail/clock.png";
+import disabledBack from "../../assets/images/detail/back_disabled.png";
+import back from "../../assets/images/detail/back.png";
+import disabledNext from "../../assets/images/detail/next_disabled.png";
+import next from "../../assets/images/detail/next.png";
+
+// Menu Image
+import image1 from "../../assets/images/menu/coffee.png";
 
 function Shop() {
   const { id } = useParams();
   const [shop, setShop] = useState(storeData[id]);
   return (
     <main id={"ShopContainer"}>
-      <div className={"ShopLeftContents"}>
+      <div className={"ShopLeftContainer"}>
         <h1>{shop.name}</h1>
         <div className={"ShopDetail"}>
           <div className={"LocationContainer"}>
@@ -33,7 +40,32 @@ function Shop() {
         <button className={"BtnHomepage"}>Homepage</button>
       </div>
       <div className={"ShopRightContainer"}>
-        <div className={"MenuCard"}></div>
+        {shop.menus.map((menu, index) => (
+          <div key={index} className={"MenuCard"}>
+            <div className={"BtnBack"}>
+              <img
+                src={index === 0 ? disabledBack : back}
+                alt={"back button"}
+              />
+            </div>
+            <div className={"MenuDetail"}>
+              <img src={image1} alt={menu.name} />
+              <div className={"MenuName"}>
+                <span>{menu.name}</span>
+                <div className={"MenuTag"}>
+                  <span>대표</span>
+                </div>
+              </div>
+              <span>{menu.price} won</span>
+            </div>
+            <div className={"BtnNext"}>
+              <img
+                src={index === shop.menus.length - 1 ? disabledNext : next}
+                alt={"back button"}
+              />
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
