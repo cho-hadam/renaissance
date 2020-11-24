be99b4ddad00277c00d0ea9529b71dba0e2cc0a0
jimin-oh <wlals6828@daum.net>
2020-11-25 08:47:18 +0900

    API, Database

--- client/src/Components/Screens/Rank.js
+++ client/src/Components/Screens/Rank.js
@@ -39,6 +39,8 @@ function Rank() {
     });
   }, []);
 
+  
+
   return (
     <main id={"RankContainer"}>
       <div className={"TopRankContainer"}>
---
+++ server.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true}));
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    database : conf.database,
+    port :  conf.port
+
+});
+con.connect();
+
+app.get('/api', (req,res) =>{
+   
+    con.query(
+        "select * from game_score order by score desc",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+        );
+});
+
+app.listen(port ,  ()=> console.log(`Listening on port ${port}`));
