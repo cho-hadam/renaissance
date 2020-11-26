d68901bcce46f479a0686f835e85be046e724ff5
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-11-27 04:36:21 +0900

    Home Desc, Icon, Title

--- src/Components/Screens/Home.js
+++ src/Components/Screens/Home.js
@@ -14,11 +14,9 @@ function Home(props) {
         <div className={"LeftContent"}>
           <h1>Renaissance</h1>
           <p className={"HomeDescription"}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            justo enim, ultricies id est nec, eleifend commodo justo. Donec vel
-            felis lacus. Nam eget risus vitae mauris molestie posuere. Praesent
-            facilisis finibus orci id sagittis. Nullam a suscipit neque, eget
-            sollicitudin urna.
+            {
+              "신사리 상권은 별이 내린 곳에서 태어난 영웅의 이야기가 살아 숨 쉬는 곳, 저마다 꿈을 가진 언젠가 밝게 빛날 별과 같은 사람들이 모이는 곳, 도림천에 흐르는 반짝이는 별빝의 물길을 따라 시간이 흐르는 곳입니다. 별빛신사리 상권 홍보를 위해 제작된 웹사이트로, 주어진 음식 사진을 보고 상점을 맞추는 게임과 상점 정보들을 확인할 수 있는 리스트를 제공하고 있습니다."
+            }
           </p>
           <button className={"BtnGame"} onClick={clickHandle}>
             <Link to={"/Game"} className={"LinkGame"}>
